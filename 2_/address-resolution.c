da0eb519ffc9ef267bb8656264810c9030ffc2d9
yoonsoo <fhkaldh@naver.com>
2021-03-21 17:10:42 +0900

    assignment 2

---
+++ 2_/address-resolution.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int printAddr(struct sockaddr_in *myAddr)
+{
+    int port;
+    char txt[INET_ADDRSTRLEN];
+    port = ntohs(myAddr->sin_port);
+    // copy inet_ntoa result to txt
+    strcpy(txt, inet_ntoa(myAddr->sin_addr));
+    printf("IP : %s, Port : %d \n", txt, port);
+    return 0;
+}
+
+int main(char argc, char **argv)
+{
+    char *sampleIP = "127.0.0.1";
+    int port = 9002;
+
+    struct sockaddr_in sockAddr1, sockAddr2, sockAddr3;
+
+    sockAddr1.sin_family = AF_INET;
+    sockAddr1.sin_addr.s_addr = inet_addr(sampleIP);
+    sockAddr1.sin_port = htons(port);
+
+    sockAddr2.sin_family = AF_INET;
+    inet_aton(sampleIP, &(sockAddr2.sin_addr));
+    sockAddr2.sin_port = htons(port);
+
+    sockAddr3.sin_family = AF_INET;
+    inet_pton(AF_INET, sampleIP, &(sockAddr3.sin_addr));
+    sockAddr3.sin_port = htons(port);
+
+    printAddr(&sockAddr1);
+    printAddr(&sockAddr2);
+    printAddr(&sockAddr3);
+
+    printf("================ ntoa ================\n");
+    printf("IP : %s \n", inet_ntoa(sockAddr1.sin_addr));
+    printf("IP : %s \n", inet_ntoa(sockAddr2.sin_addr));
+    printf("IP : %s \n", inet_ntoa(sockAddr3.sin_addr));
+
+    return 0;
+}
